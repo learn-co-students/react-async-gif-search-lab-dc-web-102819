b507e3199b8be3299f44c9290cbb262ef7106aac
jdromero88 <github email address>
2020-01-10 14:54:17 -0500

    Done.

--- src/containers/GifListContainer.js
+++ src/containers/GifListContainer.js
@@ -2,14 +2,14 @@ import React, {PureComponent} from 'react'
 import GifList from '../components/GifList'
 import GifSearch from '../components/GifSearch'
 
-const URL = 'https://api.giphy.com/v1/gifs/search?q=dolphin&api_key=aQEbryphgmP82CU8FTIT8hAV9vV7fva4&rating=g'
+const URL = 'https://api.giphy.com/v1/gifs/search?q=dolphin&limit=3&api_key=aQEbryphgmP82CU8FTIT8hAV9vV7fva4&rating=g'
 
 class GifListContainer extends PureComponent{
-  originalGifs = null
   constructor(){
     super()
     this.state = {
-      gifs: [],
+      allGifs: [],
+      filterGifs:[],
       searchQuery: ''
     }
   }
@@ -17,8 +17,9 @@ class GifListContainer extends PureComponent{
     fetch(URL)
     .then(res =>res.json())
     .then(gifs => {
-      this.originalGifs = gifs.data.splice(-3)
-      return this.setState({gifs: this.originalGifs})
+      // console.log()
+      this.setState({allGifs: gifs.data})
+      return this.setState({filterGifs: this.state.allGifs})
     })
     .catch(err => console.warn(err.message))
   }
@@ -30,11 +31,12 @@ class GifListContainer extends PureComponent{
     this.searchHelper()
   }
   searchHelper = () => {
-    let copyGifs = this.state.gifs.filter(gif => gif.title.toLowerCase() === this.state.searchQuery.toLowerCase())
-    if(copyGifs.length === 0){
-      this.setState({gifs: this.originalGifs})
+    const test = this.state.allGifs.filter(gif => gif.title.toLowerCase() === this.state.searchQuery.toLowerCase())
+    // console.log(copy)
+    if(test.length === 0){
+      this.setState({filterGifs: this.state.allGifs})
     }else{
-      this.setState({gifs: copyGifs})
+      this.setState({filterGifs: test})
     }
   }
 
@@ -44,7 +46,7 @@ class GifListContainer extends PureComponent{
         <GifSearch submitHandler={this.submitHandler} onChange={this.onChange}/>
         <div className='ui divider'></div>
         <div className='ui grid'>
-          {this.state.gifs.map(gif => <GifList key={gif.id} gif={gif}/>)}
+          {this.state.filterGifs.map(gif => <GifList key={gif.id} gif={gif}/>)}
         </div>
       </div>
     )
